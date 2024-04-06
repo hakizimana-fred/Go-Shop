a4b04bda154d3ed65fb7a35b82b2c95ef8c24bae
Hakizimana-fred <hakifred20@gmail.com>
2024-04-06 14:45:04 +0300

    fetch all products

--- controllers/products.controller.go
+++ controllers/products.controller.go
@@ -1,8 +1,14 @@
 package controllers
 
 import (
-	"fmt"
+	"context"
+	"ecommerce-project/database"
+	"ecommerce-project/seeder"
+	"ecommerce-project/utils"
 	"net/http"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func ProductsController(w http.ResponseWriter, r *http.Request) {
@@ -14,6 +20,28 @@ func ProductsController(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getProducts(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Fetching all products from db")
+func getProducts(w http.ResponseWriter, _ *http.Request) error {
+	client, err := database.MongoConnect()
+	var products []*seeder.Product
+
+	if err != nil {
+		return err
+	}
+	collection := client.Database(database.Database).Collection(string(database.ProductsCollection))
+
+	cur, err := collection.Find(context.Background(), bson.D{
+		primitive.E{},
+	})
+
+	for cur.Next(context.Background()) {
+		var p seeder.Product
+		err := cur.Decode(&p)
+
+		if err != nil {
+			return nil
+		}
+		products = append(products, &p)
+	}
+
+	return utils.WriteJson(w, http.StatusOK, products)
 }
